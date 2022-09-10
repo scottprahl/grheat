42840427d46989f6ecb7ef0abe439533b68faec7
Scott Prahl <scott@prahls.com>
2022-09-10 14:27:19 -0700

    change to object style

--- grheat/point_source.py
+++ grheat/point_source.py
@@ -13,168 +13,192 @@ Typical usage::
     import numpy as np
     import matplotlib.pyplot as plt
 
-    r = np.array([0, 0, 0])
-    rp = np.array([0, 0, 1])
-    t = np.linspace(0, 2, 100)
-    tp = 0
+    t = np.linspace(0, 500, 100) / 1000   # seconds
+    mua = 0.25 * 1000                     # 1/m
+    x,y,z = 0,0,0                         # meters
+    xp,yp,zp = 0,0,0.001                  # meters
+    t_pulse = 0.100                       # seconds
 
-    T =
-    plt.plot(t, T, color='blue')
+    medium = new grheat.Point()
+    T = medium.pulsed(x,y,z,t,xp,yp,zp,t_pulse)
+
+    plt.plot(t * 1000, T, color='blue')
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Temperature Increase (°C)")
+    plt.title("1J pulse lasting %.0f ms" % t_pulse)
     plt.show()
-
 """
 
 import scipy.special
 import numpy as np
 
-__all__ = ('instantaneous',
-           'continuous',
-           'pulsed',
-           )
-
-
 water_heat_capacity = 4.184 * 1e6           # J/degree / m**3
 water_thermal_diffusivity = 0.14558 * 1e-6  # m**2/s
 
 
-def _instantaneous(x, y, z, t, xp, yp, zp, tp):
+class Point:
     """
-    Calculate temperature rise due to a 1J instant point source at time t.
+    Green's function heat transfer solutions for point source in infinite media.
 
-    Carslaw and Jaeger page 256, 10.2(2)
+    Typical usage::
 
-    Parameters:
-        x, y, z: location for desired temperature [meters]
-        t: time of desired temperature [seconds]
-        xp, yp, zp: location of point source [meters]
-        tp: time of source impulse [seconds]
+        import grheat
+        import numpy as np
+        import matplotlib.pyplot as plt
 
-    Returns:
-        normalized temperature
+        t = np.linspace(0, 500, 100) / 1000   # seconds
+        mua = 0.25 * 1000                     # 1/m
+        x,y,z = 0,0,0                         # meters
+        xp,yp,zp = 0,0,0.001                  # meters
+        t_pulse = 0.100                       # seconds
+
+        medium = new grheat.Point()
+        T = medium.pulsed(x,y,z,t,xp,yp,zp,t_pulse)
+
+        plt.plot(t * 1000, T, color='blue')
+        plt.xlabel("Time (ms)")
+        plt.ylabel("Temperature Increase (°C)")
+        plt.title("1J pulse lasting %.0f ms" % t_pulse)
+        plt.show()
     """
-    if t <= tp:
-        return 0
 
-    kappa = water_thermal_diffusivity  # m**2/s
-    rho_cee = water_heat_capacity      # J/degree/m**3
-    r = np.sqrt((x - xp)**2 + (y - yp)**2 + (z - zp)**2)
-    factor = rho_cee * 8 * (np.pi * kappa * (t - tp))**1.5
-    return 1 / factor * np.exp(-r**2 / (4 * kappa * (t - tp)))
+    def __init__(self,
+                 diffusivity=water_thermal_diffusivity,
+                 capacity=water_heat_capacity,
+                 boundary='infinite'):
+        self.diffusivity = diffusivity     # m**2/s
+        self.capacity = capacity           # J/degree/kg
+        self.boundary = boundary.lower()   # infinite, adiabatic, constant
 
+    def _instantaneous(self, x, y, z, t, xp, yp, zp, tp):
+        """
+        Calculate temperature rise due to a 1J instant point source at time t.
 
-def instantaneous(x, y, z, t, xp, yp, zp, tp):
-    """
-    Calculate temperature rise due to a 1J instant point source at time(s) t.
+        Carslaw and Jaeger page 256, 10.2(2)
 
-    Parameters:
-        x, y, z: location for desired temperature [meters]
-        t: time(s) of desired temperature [seconds]
-        xp, yp, zp: location of point source [meters]
-        tp: time of source impulse [seconds]
+        Parameters:
+            x, y, z: location for desired temperature [meters]
+            t: time of desired temperature [seconds]
+            xp, yp, zp: location of point source [meters]
+            tp: time of source impulse [seconds]
 
-    Returns:
-        Temperature Increase [°C]
-    """
-    if np.isscalar(t):
-        T = _instantaneous(x, y, z, t, xp, yp, zp, tp)
+        Returns:
+            normalized temperature
+        """
+        if t <= tp:
+            return 0
 
-    else:
-        T = np.empty_like(t)
-        for i, tt in enumerate(t):
-            T[i] = _instantaneous(x, y, z, tt, xp, yp, zp, tp)
-    return T
+        r = np.sqrt((x - xp)**2 + (y - yp)**2 + (z - zp)**2)
+        factor = self.capacity * 8 * (np.pi * self.diffusivity * (t - tp))**1.5
+        return 1 / factor * np.exp(-r**2 / (4 * self.diffusivity * (t - tp)))
 
+    def instantaneous(self, x, y, z, t, xp, yp, zp, tp):
+        """
+        Calculate temperature rise due to a 1J instant point source at time(s) t.
 
-def _continuous(x, y, z, t, xp, yp, zp):
-    """
-    Calculate temperature rise of a 1W continuous point source at time t.
+        Parameters:
+            x, y, z: location for desired temperature [meters]
+            t: time(s) of desired temperature [seconds]
+            xp, yp, zp: location of point source [meters]
+            tp: time of source impulse [seconds]
 
-    Carslaw and Jaeger page 261, 10.4(2)
+        Returns:
+            Temperature Increase [°C]
+        """
+        if np.isscalar(t):
+            T = self._instantaneous(x, y, z, t, xp, yp, zp, tp)
 
-    Parameters:
-        x, y, z: location for desired temperature [meters]
-        t: time of desired temperature [seconds]
-        xp, yp, zp: location of point source [meters]
+        else:
+            T = np.empty_like(t)
+            for i, tt in enumerate(t):
+                T[i] = self._instantaneous(x, y, z, tt, xp, yp, zp, tp)
+        return T
 
-    Returns:
-        Temperature Increase [°C]
-    """
-    if t <= 0:
-        return 0
+    def _continuous(self, x, y, z, t, xp, yp, zp):
+        """
+        Calculate temperature rise of a 1W continuous point source at time t.
 
-    kappa = water_thermal_diffusivity  # m**2 / s
-    rho_cee = water_heat_capacity      # J/degree/m**3
-    r = np.sqrt((x - xp)**2 + (y - yp)**2 + (z - zp)**2)
-    factor = 1 / rho_cee / (4 * np.pi * kappa * r)
-    T = factor * scipy.special.erfc(r / np.sqrt(4 * kappa * t))
-    return T
+        Carslaw and Jaeger page 261, 10.4(2)
 
+        Parameters:
+            x, y, z: location for desired temperature [meters]
+            t: time of desired temperature [seconds]
+            xp, yp, zp: location of point source [meters]
 
-def continuous(x, y, z, t, xp, yp, zp):
-    """
-    Calculate temperature rise of a 1W continuous point source.
+        Returns:
+            Temperature Increase [°C]
+        """
+        if t <= 0:
+            return 0
 
-    The point source turns on at t=0.
+        r = np.sqrt((x - xp)**2 + (y - yp)**2 + (z - zp)**2)
+        factor = 1 / self.capacity / (4 * np.pi * self.diffusivity * r)
+        T = factor * scipy.special.erfc(r / np.sqrt(4 * self.diffusivity * t))
+        return T
 
-    Parameters:
-        x, y, z: location for desired temperature [meters]
-        t: time(s) of desired temperature [seconds]
-        xp, yp, zp: location of point source [meters]
+    def continuous(self, x, y, z, t, xp, yp, zp):
+        """
+        Calculate temperature rise of a 1W continuous point source.
 
-    Returns:
-        Temperature Increase [°C]
-    """
-    if np.isscalar(t):
-        T = _continuous(x, y, z, t, xp, yp, zp)
+        The point source turns on at t=0.
 
-    else:
-        T = np.empty_like(t)
-        for i, tt in enumerate(t):
-            T[i] = _continuous(x, y, z, tt, xp, yp, zp)
-    return T
+        Parameters:
+            x, y, z: location for desired temperature [meters]
+            t: time(s) of desired temperature [seconds]
+            xp, yp, zp: location of point source [meters]
 
+        Returns:
+            Temperature Increase [°C]
+        """
+        if np.isscalar(t):
+            T = self._continuous(x, y, z, t, xp, yp, zp)
 
-def _pulsed(x, y, z, t, xp, yp, zp, t_pulse):
-    """
-    Calculate temperature rise due to a 1J pulsed point source at time(s) t.
+        else:
+            T = np.empty_like(t)
+            for i, tt in enumerate(t):
+                T[i] = self._continuous(x, y, z, tt, xp, yp, zp)
+        return T
 
-    1J of heat deposited at (xp, yp, zp) from t=0 to t=t_pulse.
+    def _pulsed(self, x, y, z, t, xp, yp, zp, t_pulse):
+        """
+        Calculate temperature rise due to a 1J pulsed point source at time(s) t.
 
-    Parameters:
-        x, y, z: location for desired temperature [meters]
-        t: time of desired temperature [seconds]
-        xp, yp, zp: location of point source [meters]
-        t_pulse: duration of pulse [seconds]
+        1J of heat deposited at (xp, yp, zp) from t=0 to t=t_pulse.
 
-    Returns:
-        Temperature Increase [°C]
-    """
-    if t <= 0:
-        T = 0
-    else:
-        T = _continuous(x, y, z, t, xp, yp, zp)
-        if t > t_pulse:
-            T -= _continuous(x, y, z, t - t_pulse, xp, yp, zp)
-    return T / t_pulse
+        Parameters:
+            x, y, z: location for desired temperature [meters]
+            t: time of desired temperature [seconds]
+            xp, yp, zp: location of point source [meters]
+            t_pulse: duration of pulse [seconds]
 
+        Returns:
+            Temperature Increase [°C]
+        """
+        if t <= 0:
+            T = 0
+        else:
+            T = self._continuous(x, y, z, t, xp, yp, zp)
+            if t > t_pulse:
+                T -= self._continuous(x, y, z, t - t_pulse, xp, yp, zp)
+        return T / t_pulse
 
-def pulsed(x, y, z, t, xp, yp, zp, t_pulse):
-    """
-    Calculate temperature rise due to a 1J pulsed point source.
+    def pulsed(self, x, y, z, t, xp, yp, zp, t_pulse):
+        """
+        Calculate temperature rise due to a 1J pulsed point source.
 
-    Parameters:
-        x, y, z: location for desired temperature [meters]
-        t: time(s) of desired temperature [seconds]
-        xp, yp, zp: location of point source [meters]
-        t_pulse: duration of pulse [seconds]
+        Parameters:
+            x, y, z: location for desired temperature [meters]
+            t: time(s) of desired temperature [seconds]
+            xp, yp, zp: location of point source [meters]
+            t_pulse: duration of pulse [seconds]
 
-    Returns
-        Temperature Increase [°C]
-    """
-    if np.isscalar(t):
-        T = _pulsed(x, y, z, t, xp, yp, zp, t_pulse)
-    else:
-        T = np.empty_like(t)
-        for i, tt in enumerate(t):
-            T[i] = _pulsed(x, y, z, tt, xp, yp, zp, t_pulse)
-    return T
+        Returns
+            Temperature Increase [°C]
+        """
+        if np.isscalar(t):
+            T = self._pulsed(x, y, z, t, xp, yp, zp, t_pulse)
+        else:
+            T = np.empty_like(t)
+            for i, tt in enumerate(t):
+                T[i] = self._pulsed(x, y, z, tt, xp, yp, zp, t_pulse)
+        return T
--- tests/test_point.py
+++ tests/test_point.py
@@ -8,7 +8,7 @@
 
 import unittest
 import numpy as np
-import grheat.point_source as point
+import grheat
 
 # the low level tests use functions that should not be exported.  These work
 # but now that the higher level tests pass, these are skipped
@@ -17,93 +17,105 @@ import grheat.point_source as point
 class InstantaneousPoint(unittest.TestCase):
 
     def test_01_scalar(self):
+        p = grheat.Point()
         tp = 0
         t = 1
-        T = point.instantaneous(0, 0, 0, t, 0, 0, 1, tp)
+        T = p.instantaneous(0, 0, 0, t, 0, 0, 1, tp)
 
     def test_02_time_array(self):
+        p = grheat.Point()
         tp = 0
         t = np.linspace(0,10)
-        T = point.instantaneous(0, 0, 0, t, 0, 0, 1, tp)
+        T = p.instantaneous(0, 0, 0, t, 0, 0, 1, tp)
 
     def test_03_surface(self):
+        p = grheat.Point()
         tp = 0
         t = 1
         X, Y = np.meshgrid(np.arange(-5, 5, 1), np.arange(-5, 5, 1))
-        T = point.instantaneous(X, Y, 0, t, 0, 0, 1, tp)
+        T = p.instantaneous(X, Y, 0, t, 0, 0, 1, tp)
 
 
 class ContinuousPoint(unittest.TestCase):
 
     def test_01_scalar(self):
+        p = grheat.Point()
         t = 1
-        T = point.continuous(0, 0, 0, t, 0, 0, 1)
+        T = p.continuous(0, 0, 0, t, 0, 0, 1)
 
     def test_02_time_array(self):
+        p = grheat.Point()
         t = np.linspace(0,10)
-        T = point.continuous(0, 0, 0, t, 0, 0, 1)
+        T = p.continuous(0, 0, 0, t, 0, 0, 1)
 
     def test_03_surface(self):
+        p = grheat.Point()
         tp = 0
         t = 1
         X, Y = np.meshgrid(np.arange(-5, 5, 1), np.arange(-5, 5, 1))
-        T = point.continuous(X, Y, 0, t, 0, 0, 1)
+        T = p.continuous(X, Y, 0, t, 0, 0, 1)
 
 
 class PulsedPoint(unittest.TestCase):
 
     def test_01_scalar(self):
+        p = grheat.Point()
         t_pulse = 0.5
         t = 1
         zp = 0.001  # meters
-        T = point.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
+        T = p.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
 
     def test_02_time_array(self):
+        p = grheat.Point()
         t_pulse = 0.5
         t = np.linspace(0,10)
         zp = 0.001  # meters
-        T = point.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
+        T = p.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
 
     def test_03_surface(self):
+        p = grheat.Point()
         t_pulse = 0.5
         t = 1
         zp = 0.001  # meters
         X, Y = np.meshgrid(np.arange(-0.005, 0.005, 0.001), np.arange(-0.005, 0.005, 0.001))
-        T = point.pulsed(X, Y, 0, t, 0, 0, zp, t_pulse)
+        T = p.pulsed(X, Y, 0, t, 0, 0, zp, t_pulse)
 
 class InstantVsPulsed(unittest.TestCase):
 
     def test_01_instant(self):
         """Short pulse result should be same as instantaneous source."""
+        p = grheat.Point()
         t_pulse = 0.00001
         tp = 0
         t = 1
         zp = 0.001  # meters
-        T1 = point.instantaneous(0, 0, 0, t, 0, 0, zp, tp)
-        T2 = point.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
+        T1 = p.instantaneous(0, 0, 0, t, 0, 0, zp, tp)
+        T2 = p.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
         self.assertAlmostEqual(T1, T2, delta=0.001)
 
     def test_02_instant(self):
         """Short pulse result should be same as instantaneous source."""
+        p = grheat.Point()
         t_pulse = 0.00001
         tp = 0
         t = np.linspace(0,10)
         zp = 0.001  # meters
-        T1 = point.instantaneous(0, 0, 0, t, 0, 0, zp, tp)
-        T2 = point.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
+        T1 = p.instantaneous(0, 0, 0, t, 0, 0, zp, tp)
+        T2 = p.pulsed(0, 0, 0, t, 0, 0, zp, t_pulse)
         self.assertAlmostEqual(T1[3], T2[3], delta=0.001)
         self.assertAlmostEqual(T1[13], T2[13], delta=0.001)
 
     def test_03_instant(self):
         """Short pulse result should be same as instantaneous source."""
+        p = grheat.Point()
         t_pulse = 0.00001
         tp = 0
         t = 1
         zp = 0.001  # meters
         X, Y = np.meshgrid(np.arange(-0.005, 0.005, 0.001), np.arange(-0.005, 0.005, 0.001))
 
-        T1 = point.instantaneous(X, Y, 0, t, 0, 0, zp, tp)
-        T2 = point.pulsed(X, Y, 0, t, 0, 0, zp, t_pulse)
+        T1 = p.instantaneous(X, Y, 0, t, 0, 0, zp, tp)
+        T2 = p.pulsed(X, Y, 0, t, 0, 0, zp, t_pulse)
         self.assertAlmostEqual(T1[0,3], T2[0,3], delta=0.001)
         self.assertAlmostEqual(T1[3,1], T2[3,1], delta=0.001)
 
