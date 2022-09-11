786e08213b91ff10b19a90119f0d6ff596988cd8
Scott Prahl <scott@prahls.com>
2022-09-11 12:29:25 -0700

    add boundary testing for point

--- tests/test_point.py
+++ tests/test_point.py
@@ -132,5 +132,74 @@ class InstantVsPulsed(unittest.TestCase):
         self.assertAlmostEqual(T1[0,3], T2[0,3], delta=0.001)
         self.assertAlmostEqual(T1[3,1], T2[3,1], delta=0.001)
 
+class ConstantBoundary(unittest.TestCase):
+
+    def test_01_constant(self):
+        """Surface temperature should be zero."""
+        xp, yp, zp = 0, 0, 0.001      # meters
+        point = grheat.Point(xp, yp, zp, boundary='constant')
+        t_pulse = 1
+        t = 2
+        T = point.pulsed(0, 0, 0, t, t_pulse)
+        self.assertEqual(T, 0)
+
+    def test_02_constant(self):
+        """Surface temperature should be zero at all times."""
+        xp, yp, zp = 0, 0, 0.001      # meters
+        point = grheat.Point(xp, yp, zp, boundary='constant')
+        t_pulse = 1
+        t = np.linspace(0,10)
+        T = point.pulsed(0, 0, 0, t, t_pulse)
+        self.assertEqual(T[3], 0)
+        self.assertEqual(T[13], 0)
+
+    def test_03_constant(self):
+        """Short pulse result should be same as instantaneous source."""
+        xp, yp, zp = 0, 0, 0.0001      # meters
+        point = grheat.Point(xp, yp, zp, boundary='constant')
+        t_pulse = 1
+        t = 1.1
+        X, Y = np.meshgrid(np.arange(-0.0005, 0.0005, 0.0001), np.arange(-0.0005, 0.0005, 0.0001))
+        T = point.pulsed(X, Y, 0, t, t_pulse)
+        self.assertEqual(T[0,3], 0)
+        self.assertEqual(T[3,1], 0)
+
+
+class AdiabaticBoundary(unittest.TestCase):
+
+    def test_01_adiabatic(self):
+        """Temperature should be equal above and below."""
+        xp, yp, zp = 0, 0, 0.001      # meters
+        point = grheat.Point(xp, yp, zp, boundary='adiabatic')
+        t_pulse = 1
+        t = 2
+        T1 = point.pulsed(0, 0, +0.0001, t, t_pulse)
+        T2 = point.pulsed(0, 0, -0.0001, t, t_pulse)
+        self.assertAlmostEqual(T1, T2, delta=1e-8)
+
+    def test_02_adiabatic(self):
+        """Temperature should be equal above and below at all times."""
+        xp, yp, zp = 0, 0, 0.001      # meters
+        point = grheat.Point(xp, yp, zp, boundary='adiabatic')
+        t_pulse = 1
+        t = np.linspace(0,2)
+        T1 = point.pulsed(0, 0, +0.0001, t, t_pulse)
+        T2 = point.pulsed(0, 0, -0.0001, t, t_pulse)
+        self.assertAlmostEqual(T1[3], T2[3], delta=1e-8)
+        self.assertAlmostEqual(T1[13], T2[13], delta=1e-8)
+
+    def test_03_adiabatic(self):
+        """Temperature should be equal above and below."""
+        xp, yp, zp = 0, 0, 0.001      # meters
+        point = grheat.Point(xp, yp, zp, boundary='adiabatic')
+        t_pulse = 1
+        t = 1.1
+        X, Y = np.meshgrid(np.arange(-0.0005, 0.0005, 0.0001), np.arange(-0.0005, 0.0005, 0.0001))
+        T1 = point.pulsed(X, Y, +0.0001, t, t_pulse)
+        T2 = point.pulsed(X, Y, -0.0001, t, t_pulse)
+        self.assertAlmostEqual(T1[0,3], T2[0,3], delta=1e-8)
+        self.assertAlmostEqual(T1[3,1], T2[3,1], delta=1e-8)
+
+
 if __name__ == '__main__':
     unittest.main()
