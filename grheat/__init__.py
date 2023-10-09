0da0bbd6d8d33ed90e6cf09b9d9f2501bcbc93bd
Scott Prahl <scott@prahls.com>
2023-10-08 17:35:36 -0700

    fix copyright and url

--- grheat/__init__.py
+++ grheat/__init__.py
@@ -7,9 +7,9 @@ More documentation at <https://grheat.readthedocs.io>
 __version__ = '0.1.0'
 __author__ = 'Scott Prahl'
 __email__ = 'scott.prahl@oit.edu'
-__copyright__ = 'Copyright 2022, Scott Prahl'
+__copyright__ = '2022-23, Scott Prahl'
 __license__ = 'MIT'
-__url__ = 'https://github.com/scottprahl/grheat.git'
+__url__ = 'https://github.com/scottprahl/grheat'
 
 from .point_source import *
 from .line_source import *
