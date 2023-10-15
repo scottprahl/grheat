e6c79b3b21ff705ea5a741690d7a65d824b8004a
Scott Prahl <scott@prahls.com>
2023-10-15 13:43:54 -0700

    more tests, cleanup

--- tests/test_point.py
+++ tests/test_point.py
@@ -14,6 +14,10 @@ import grheat
 class InstantaneousPoint(unittest.TestCase):
 
     def test_01_scalar(self):
+        """
+        Test if the method `instantaneous` can handle scalar input and
+        compute the temperature at a single point in space at a given time.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         tp = 0
@@ -21,6 +25,10 @@ class InstantaneousPoint(unittest.TestCase):
         T = point.instantaneous(0, 0, 0, t, tp)
 
     def test_02_time_array(self):
+        """
+        Test if the method `instantaneous` can handle an array of time values
+        and compute the temperature at a single point in space across these times.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         tp = 0
@@ -28,6 +36,10 @@ class InstantaneousPoint(unittest.TestCase):
         T = point.instantaneous(0, 0, 0, t, tp)
 
     def test_03_surface(self):
+        """
+        Test if the method `instantaneous` can handle meshgrid input for x and y
+        coordinates and compute the temperature across a surface at a given time.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         tp = 0
@@ -35,22 +47,93 @@ class InstantaneousPoint(unittest.TestCase):
         X, Y = np.meshgrid(np.arange(-5, 5, 1), np.arange(-5, 5, 1))
         T = point.instantaneous(X, Y, 0, t, tp)
 
+    def test_invalid_boundary_01(self):
+        """
+        Test if passing an invalid boundary value 'bad_value' raises a ValueError.
+        """
+        with self.assertRaises(ValueError):
+            grheat.Point(0, 0, 0.001, boundary='bad_value')
+
+    def test_04_surface(self):
+        """
+        Verify that we still get an array zeros if t=tp.
+        """
+        tp = 0                          # seconds
+        t = tp                          # seconds
+        xp, yp, zp = 0, 0.0001, 0.001   # m
+        x = 0                           # m
+        y = 0                           # m
+        z = np.linspace(0, 0.002, 101)  # m
+        zz = 1000 * z                   # mm
+
+        point = grheat.Point(xp, yp, zp, boundary='zero')
+        T = point.instantaneous(x, y, z, t, tp)
+        self.assertIsInstance(T, np.ndarray)     # is an array
+        self.assertTrue(np.all(np.equal(T, 0)))  # are all zeros
+        self.assertEqual(T.shape, z.shape)       # same shape
+
+    def test_05_surface(self):
+        """
+        Verify that we still get an array zeros if t<tp.
+        """
+        tp = 1                          # seconds
+        t = 0.5                         # seconds
+        xp, yp, zp = 0, 0.0001, 0.001   # m
+        x = 0                           # m
+        y = 0                           # m
+        z = np.linspace(0, 0.002, 101)  # m
+        zz = 1000 * z                   # mm
+
+        point = grheat.Point(xp, yp, zp, boundary='zero')
+        T = point.instantaneous(x, y, z, t, tp)
+        self.assertIsInstance(T, np.ndarray)     # is an array
+        self.assertTrue(np.all(np.equal(T, 0)))  # are all zeros
+        self.assertEqual(T.shape, z.shape)       # same shape
+
+    def test_06_surface(self):
+        """
+        Verify that we still get an array zeros if t<tp.
+        """
+        tp = 1                          # seconds
+        t = 0.5                         # seconds
+        xp, yp, zp = 0, 0.0001, 0.001   # m
+        x = 0                           # m
+        y = 0                           # m
+        z = 0.001                       # m
+        zz = 1000 * z                   # mm
+
+        point = grheat.Point(xp, yp, zp, boundary='zero')
+        T = point.instantaneous(x, y, z, t, tp)
+        self.assertEqual(T, 0)
+
 
 class ContinuousPoint(unittest.TestCase):
 
     def test_01_scalar(self):
+        """
+        Test if the method `continuous` can handle scalar input and
+        compute the temperature at a single point in space at a given time.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t = 1
         T = point.continuous(0, 0, 0, t)
 
     def test_02_time_array(self):
+        """
+        Test if the method `continuous` can handle an array of time values
+        and compute the temperature at a single point in space across these times.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t = np.linspace(0, 10)
         T = point.continuous(0, 0, 0, t)
 
     def test_03_surface(self):
+        """
+        Test if the method `continuous` can handle meshgrid input for x and y
+        coordinates and compute the temperature across a surface at a given time.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         tp = 0
@@ -59,6 +142,11 @@ class ContinuousPoint(unittest.TestCase):
         T = point.continuous(X, Y, 0, t)
 
     def test_04_scalar(self):
+        """
+        Test if the method `continuous`'s output for a scalar input is
+        consistent with the averaged output of method `instantaneous`
+        over a range of time points.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t = 1
@@ -69,10 +157,63 @@ class ContinuousPoint(unittest.TestCase):
         T2 = T.sum() / N
         self.assertAlmostEqual(T1, T2, delta=0.01)
 
+    def test_05_surface(self):
+        """
+        Verify that we still get an array zeros if t=tp.
+        """
+        t = -1                          # seconds
+        xp, yp, zp = 0, 0.0001, 0.001   # m
+        x = 0                           # m
+        y = 0                           # m
+        z = np.linspace(0, 0.002, 101)  # m
+        zz = 1000 * z                   # mm
+
+        point = grheat.Point(xp, yp, zp, boundary='zero')
+        T = point.continuous(x, y, z, t)
+        self.assertIsInstance(T, np.ndarray)     # is an array
+        self.assertTrue(np.all(np.equal(T, 0)))  # are all zeros
+        self.assertEqual(T.shape, z.shape)       # same shape
+
+    def test_06_surface(self):
+        """
+        Verify that we still get an array zeros if t<tp.
+        """
+        t = 0                           # seconds
+        xp, yp, zp = 0, 0.0001, 0.001   # m
+        x = 0                           # m
+        y = 0                           # m
+        z = np.linspace(0, 0.002, 101)  # m
+        zz = 1000 * z                   # mm
+
+        point = grheat.Point(xp, yp, zp, boundary='zero')
+        T = point.continuous(x, y, z, t)
+        self.assertIsInstance(T, np.ndarray)     # is an array
+        self.assertTrue(np.all(np.equal(T, 0)))  # are all zeros
+        self.assertEqual(T.shape, z.shape)       # same shape
+
+    def test_07_surface(self):
+        """
+        Verify that we still get an array zeros if t<tp.
+        """
+        t = 0                           # seconds
+        xp, yp, zp = 0, 0.0001, 0.001   # m
+        x = 0                           # m
+        y = 0                           # m
+        z = 0.001                       # m
+        zz = 1000 * z                   # mm
+
+        point = grheat.Point(xp, yp, zp, boundary='zero')
+        T = point.continuous(x, y, z, t)
+        self.assertEqual(T, 0)
 
 class PulsedPoint(unittest.TestCase):
 
     def test_01_scalar(self):
+        """
+        Test if the method `pulsed` can handle scalar input and
+        compute the temperature at a single point in space at a given time,
+        following a pulse at a specified earlier time.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t_pulse = 0.5
@@ -80,6 +221,11 @@ class PulsedPoint(unittest.TestCase):
         T = point.pulsed(0, 0, 0, t, t_pulse)
 
     def test_02_time_array(self):
+        """
+        Test if the method `pulsed` can handle an array of time values
+        and compute the temperature at a single point in space across these times,
+        following a pulse at a specified earlier time.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t_pulse = 0.5
@@ -87,6 +233,11 @@ class PulsedPoint(unittest.TestCase):
         T = point.pulsed(0, 0, 0, t, t_pulse)
 
     def test_03_surface(self):
+        """
+        Test if the method `pulsed` can handle meshgrid input for x and y
+        coordinates and compute the temperature across a surface at a given time,
+        following a pulse at a specified earlier time.
+        """
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t_pulse = 0.5
@@ -98,7 +249,7 @@ class PulsedPoint(unittest.TestCase):
 class InstantVsPulsed(unittest.TestCase):
 
     def test_01_instant(self):
-        """Short pulse result should be same as instantaneous source."""
+        """Short pulse result should be same as instantaneous source for scalars."""
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t_pulse = 0.00001
@@ -109,7 +260,7 @@ class InstantVsPulsed(unittest.TestCase):
         self.assertAlmostEqual(T1, T2, delta=0.001)
 
     def test_02_instant(self):
-        """Short pulse result should be same as instantaneous source."""
+        """Short pulse result should be same as instantaneous source for arrays."""
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t_pulse = 0.00001
@@ -121,7 +272,7 @@ class InstantVsPulsed(unittest.TestCase):
         self.assertAlmostEqual(T1[13], T2[13], delta=0.001)
 
     def test_03_instant(self):
-        """Short pulse result should be same as instantaneous source."""
+        """Short pulse result should be same as instantaneous source for mesh."""
         xp, yp, zp = 0, 0, 0.001      # meters
         point = grheat.Point(xp, yp, zp)
         t_pulse = 0.00001
